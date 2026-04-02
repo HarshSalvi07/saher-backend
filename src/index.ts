3184a0dee2150ee97a879cc2f331641cbde95741
Harsh Salvi <harshsalvi250@gmail.com>
2026-04-02 16:33:39 +0530

    Integrated with current structure

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import authRouter from "./auth/auth.routes.js";
 import connectDb from "./database/connection.js";
 import { protectedRoute } from "./libs/middleware/protected-route.js";
 import attendenceRouter from "./attendence/attendence.route.js"
-import programRoutes from "./event.routes.js";
+import programRoutes from "./events/event/event.routes.js";
 import uploadRouter from "./upload/upload.routes.js";
 import errorHandler from "./libs/middleware/error-handler.js";
 
