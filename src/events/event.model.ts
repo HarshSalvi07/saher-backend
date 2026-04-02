55b3bc78f1328a818b610be87faf96333fb8b230
Harsh Salvi <harshsalvi250@gmail.com>
2026-04-02 15:43:35 +0530

    More model changes added for events

--- src/events/event.model.ts
+++ src/events/event.model.ts
@@ -1,33 +1,35 @@
 import mongoose from "mongoose";
 import { required } from "zod/mini";
 
-const programSchema = new mongoose.Schema({ 
-  id:{
-    type:String,
-    required:true
+const programSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
   },
-  string:{
-    type:String,
-    required:true
+  string: {
+    type: String,
+    required: true,
   },
   title: {
-    type:String,
-    required:true
+    type: String,
+    required: true,
   },
 
-  description:{
-    type:String,
-    required:true
+  description: {
+    type: String,
+    required: true,
   },
 
-  date:{
-    type:Date,
-    required:true
+  date: {
+    type: Date,
+    required: true,
   },
 
-  manager:{
-    type:String,
-    required:true
-  }})
+  manager: {
+    type: String,
+    required: true,
+  },
+});
 
-export const Program = mongoose.model("Program", programSchema)
+export type eventType = mongoose.InferSchemaType<typeof programSchema>;
+export const Program = mongoose.model("Program", programSchema);
